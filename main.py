aad6d771b1401ff6ab8c4358fe2f1b13b86cac49
isabelbosquet <isabosdev@gmail.com>
2023-09-04 18:41:03 +0100

    Nueva prueba de ejecución con uso de variables de entorno

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
 
-# Configurar credenciales
-client_id = '13da623734754daeaabe8fa9b7502075'
-client_secret = 'abc'
+load_dotenv()
+
+client_id = os.environ.get('SPOTIFY_CLIENT_ID')
+client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
 # Autenticación
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # Haz una solicitud a la API, por ejemplo, buscar un álbum
-results = sp.search(q='album:Abbey Road artist:The Beatles', type='album')
+results = sp.search(q='A Night at the Opera artist:Queen', type='album')
 albums = results['albums']['items']  # Cambié 'itmes' a 'items'
 
 for album in albums:
